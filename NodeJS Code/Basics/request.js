ed9a7ae3d86fe44e82f66a4fe0fdf5f62845a9a3
Somik Datta <developersomik@gmail.com>
2020-04-08 11:16:42 +0530

    Studying request object

---
+++ NodeJS Code/Basics/request.js
@@ -0,0 +1,9 @@
+//Studying request object
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method, req.headers);
+});
+
+server.listen(3000);
