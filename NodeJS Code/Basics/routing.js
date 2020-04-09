30dbf5e0d431e5f22d29829d6196d583081b8f82
Somik Datta <developersomik@gmail.com>
2020-04-09 17:58:07 +0530

    Refactor routes into separate file

--- NodeJS Code/Basics/routing.js
+++ NodeJS Code/Basics/routing.js
@@ -1,35 +1,6 @@
 const http = require('http');
-const fs = require('fs');
+const routes = require('./routes');
 
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
-    if (url === '/') {
-        res.write('<html><head><title>Enter message</title></title><body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body></html>');
-        return res.end();
-    }
-    if (url === '/received') {
-        res.write('<html><head><title>Message sent!</title></title><body><h1>Message received</h1></body></html>');
-        return res.end();
-    }
-    if (url === "/message" && method === 'POST') {
-        const body = [];
-        req.on('data', (chunk) => {
-            body.push(chunk);
-        });
-        req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            // fs.writeFileSync('message.txt', message); -synchronous (blocking)
-            fs.writeFile('message.txt', message, (err) => {
-                // res.statusCode=302;
-                // res.setHeader('Location', '/');
-                res.writeHead(302, { 'Location': '/received' });
-                return res.end();
-            });
-
-        });
-    }
-});
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
