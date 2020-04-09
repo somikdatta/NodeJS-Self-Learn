1655e4ea644959f3d295ca52ad35e36b1f7a09ec
Somik Datta <developersomik@gmail.com>
2020-04-09 17:13:18 +0530

    Request routing

--- NodeJS Code/Basics/routing.js
+++ NodeJS Code/Basics/routing.js
@@ -8,6 +8,10 @@ const server = http.createServer((req, res) => {
         res.write('<html><head><title>Enter message</title></title><body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body></html>');
         return res.end();
     }
+    if (url === '/received') {
+        res.write('<html><head><title>Message sent!</title></title><body><h1>Message received</h1></body></html>');
+        return res.end();
+    }
     if (url === "/message" && method === 'POST') {
         const body = [];
         req.on('data', (chunk) => {
@@ -16,12 +20,15 @@ const server = http.createServer((req, res) => {
         req.on('end', () => {
             const parsedBody = Buffer.concat(body).toString();
             const message = parsedBody.split('=')[1];
-            fs.writeFileSync('message.txt', message);
-        })
-        // res.statusCode=302;
-        // res.setHeader('Location', '/');
-        res.writeHead(302, { 'Location': '/' });
-        return res.end();
+            // fs.writeFileSync('message.txt', message); -synchronous (blocking)
+            fs.writeFile('message.txt', message, (err) => {
+                // res.statusCode=302;
+                // res.setHeader('Location', '/');
+                res.writeHead(302, { 'Location': '/received' });
+                return res.end();
+            });
+
+        });
     }
 });
 
